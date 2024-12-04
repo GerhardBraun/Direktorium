d9963de1e5236c164e73aa9fa94026ea38973aa6
unknown <gerhard.braun@bistum-fulda.de>
2024-12-04 19:16:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Direktorium from './components/Direktorium';
+import React from 'react';
 
 function App() {
   return (
--- src/components/Direktorium.js
+++ src/components/Direktorium.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { Menu } from 'lucide-react';
+import React from 'react';
 
 const formatText = (text) => {
     if (!text) return '';
