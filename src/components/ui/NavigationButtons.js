36cedc92e1bfd22f291f20eeac4a012e9b0084e9
Gerhard Braun <gerhard.braun@bistum-fulda.de>
2025-08-23 13:01:51 +0200

    Verbesserung: NavigationButtons

--- src/components/ui/NavigationButtons.js
+++ src/components/ui/NavigationButtons.js
@@ -16,7 +16,7 @@ const NavigationButtons = ({ hour, onBack, onSelectHour, topButton = false, text
             case 'laudes':
                 return [
                     { label: 'Lesehore', name: 'lesehore' },
-                    { label: 'Terz/Sext/Non', name: 'terz', span: '3' }
+                    { label: 'Terz/Sext/Non', name: 'terz', span: 'col-span-3' }
                 ];
             case 'terz':
             case 'sext':
@@ -53,11 +53,12 @@ const NavigationButtons = ({ hour, onBack, onSelectHour, topButton = false, text
             alignment === 'left' ? '\u00A0←\u00A0' + singleButton
                 : alignment === 'right' ? singleButton + '\u00A0→\u00A0\u00A0\u00A0'
                     : ''
+
         return {
             alignment,
             singleButton,
             columns: (nextHours[0]?.span || nextHours[1]?.span)
-                ? '7' : ''
+                ? 'grid-cols-7' : 'grid-cols-6'
         };
     }
 
@@ -118,7 +119,7 @@ const NavigationButtons = ({ hour, onBack, onSelectHour, topButton = false, text
     }
 
     return (
-        <div className={`grid grid-cols-${columns ? columns : '6'} gap-2 mb-1`}>
+        <div className={`grid ${columns} gap-2 mb-1`}>
             <button
                 onClick={onBack}
                 className="col-span-2 p-2 rounded-sm bg-gray-100 dark:bg-gray-800
@@ -144,7 +145,7 @@ const NavigationButtons = ({ hour, onBack, onSelectHour, topButton = false, text
                         key={index}
                         onClick={() => handleHourChange(nextHour)}
                         disabled={!nextHour.name}
-                        className={`col-span-${nextHour?.span ? nextHour.span : '2'} p-2 rounded-sm text-sm
+                        className={`${nextHour?.span ? nextHour.span : 'col-span-2'} p-2 rounded-sm text-sm
                             ${nextHour.name
                                 ? 'bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 text-rubric'
                                 : 'bg-gray-50 dark:bg-gray-900 text-gray-400 dark:text-gray-600 cursor-default'}`}
