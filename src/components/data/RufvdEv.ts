f0d914b8fbd5a9d539baa9adaf71ce18fb91177f
Gerhard Braun <gerhard.braun@bistum-fulda.de>
2026-02-19 21:08:42 +0100

    Korrektur Psalm 144

--- src/components/data/RufvdEv.ts
+++ src/components/data/RufvdEv.ts
@@ -1,4 +1,4 @@
-// RufvdEv.ts - Automatisch generiert am 19.02.2026 13:17:55
+// RufvdEv.ts - Automatisch generiert am 19.02.2026 21:08:42
 // RufvdEv-Datenbank: ID -> { Stelle, Text }
 
 export const rufvdevData = {
