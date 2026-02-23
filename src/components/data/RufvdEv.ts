a1ee9d084332e098980bc6845083d520bdfdc5e7
Gerhard Braun <gerhard.braun@bistum-fulda.de>
2026-02-23 11:40:33 +0100

    Test zur Aktualisierung

--- src/components/data/RufvdEv.ts
+++ src/components/data/RufvdEv.ts
@@ -1,4 +1,4 @@
-// RufvdEv.ts - Automatisch generiert am 23.02.2026 06:43:58
+// RufvdEv.ts - Automatisch generiert am 23.02.2026 11:40:31
 // RufvdEv-Datenbank: ID -> { Stelle, Text }
 
 export const rufvdevData = {
