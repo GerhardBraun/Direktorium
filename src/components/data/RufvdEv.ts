46e36918258bb595bb7ed8bb33437b5cfcea8c0e
Gerhard Braun <gerhard.braun@bistum-fulda.de>
2026-04-02 17:10:10 +0200

    Ergänzung: lat. Vesperhymnus am Gründonnerstag

--- src/components/data/RufvdEv.ts
+++ src/components/data/RufvdEv.ts
@@ -1,4 +1,4 @@
-// RufvdEv.ts - Automatisch generiert am 02.04.2026 05:39:31
+// RufvdEv.ts - Automatisch generiert am 02.04.2026 17:10:09
 // RufvdEv-Datenbank: ID -> { Stelle, Text }
 
 export const rufvdevData = {
